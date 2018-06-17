01a3638eb10733b2a852c5ca1e47f18caffa186c
psteiwer <psteiwer@gmail.com>
2018-06-16 21:26:14 -0400

    Split js files
    ScavengingCalculator.js contains calculator utilitiy methods
    DiscordBot.js contains bot specific methods

--- js/ScavengingCalculator.js
+++ js/ScavengingCalculator.js
@@ -1,8 +1,3 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const config = require("./tw-scavenging.json");
-
-
 function calculate(a, b, c, d, sp, sw, ax, hc) {
 	console.log("a: "+a);
 	console.log("b: "+b);
@@ -141,32 +136,3 @@ function calculateTroops(haul,sp,sw,ax,hc) {
 	return [spCount,swCount,axCount,hcCount];
 }
 
-client.on("ready", () => {
-  console.log("Ready");
-});
-
-client.on("message", (message) => {
-	if(message.author.bot) return;
-	var msg = message.content;
-	if (((message.channel.guild.id!='456152753144856580') || (message.channel.name!='scavenge-calculator')) && (message.channel.guild.id!='456997367464591360')) {
-		return;
-	}
-	console.log("Message from: "+message.author.username+" ("+message.author.id+") @ "+message.channel.guild.name+" ("+message.channel.guild.id+") "+" - "+message.channel.name);
-	
-	var expr = /(A|B|C|D){1,4}\s\d*[/]\d*[/]\d*[/]\d*/i;
-	if (msg.match(expr)) {
-		msg = msg.split(" ");
-		message.channel.send(msg);
-		missions=msg[0].split("");
-		troops=msg[1].split("/");
-		console.log(missions);
-		res=calculate(msg[0].includes('a'),msg[0].includes('b'),msg[0].includes('c'),msg[0].includes('d'),troops[0],troops[1],troops[2],troops[3]);
-		console.log(res);
-		message.channel.send(res);
-	} else {
-		message.channel.send("Sorry, wrong format. Please use 'abcd 0/0/0/0'");
-	}
-    
-});
-
-client.login(config.token);
