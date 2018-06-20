73b8809bda915bab09bc2e6976684f9b3be8404b
psteiwer <psteiwer@gmail.com>
2018-06-19 23:30:03 -0400

    Sent + Received message logging

--- js/DiscordBot.js
+++ js/DiscordBot.js
@@ -10,7 +10,7 @@ function logMessageIn(message) {
 	console.log("User: "+message.author.username+" ("+message.author.id+")");
 	console.log("Sever: "+message.channel.guild.name+" ("+message.channel.guild.id+")");
 	console.log("Channel: "+message.channel.name);
-	console.log("Message: "+message.content);
+	console.log("Received: "+message.content);
 	console.log("");
 }
 
@@ -19,6 +19,11 @@ function logMessageOut(message) {
 	console.log(date.toString());
 	console.log("Sent: "+message);
 	console.log("");
+}
+
+function sendMessage(message,text) {
+	logMessageOut(text);
+	message.channel.send(text);
 }
 
 client.on("ready", () => {
@@ -37,18 +42,19 @@ client.on("message", (message) => {
 	var expr = /(A|B|C|D){1,4}\s\d*[/]\d*[/]\d*[/]\d*/i;
 	if (msg.match(expr)) {
 		msg = msg.split(" ");
-		message.channel.send(msg);
+		sendMessage(message,msg);
 		missions=msg[0].split("");
 		troops=msg[1].split("/");
 		res=utils.calcExternal(msg[0].toLowerCase().includes('a'),msg[0].toLowerCase().includes('b'),msg[0].toLowerCase().includes('c'),msg[0].toLowerCase().includes('d'),troops[0],troops[1],troops[2],troops[3]);
-		message.channel.send(res);
+		sendMessage(message,res);
 	} else if (msg=='!help') {
-		message.channel.send("Expected format is 'abcd 0/0/0/0'." +
+		messageText = "Expected format is 'abcd 0/0/0/0'." +
 				"\n'abcd' is any combination of missions that you would like to scavenge with." +
 				"\n'0/0/0/0' are the number of troops you would like to send in total." +
-				"\nTroop order is spear/sword/axe/heavy cavalry.");
+				"\nTroop order is spear/sword/axe/heavy cavalry.";
+		sendMessage(message,messageText);
 	} else {
-		message.channel.send("Sorry, wrong format. Type !help for format info.");
+		sendMessage(message,"Sorry, wrong format. Type !help for format info.");
 	}
     
 });
